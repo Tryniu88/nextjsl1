7024fc624f19461ad4c3b1fd346c9da48fec1d3d
Tryniu88 <127084934+Tryniu88@users.noreply.github.com>
2024-10-03 10:43:31 +0200

    colors

---
+++ src/app/strona3/page.js
@@ -0,0 +1,31 @@
+"use client"
+
+
+import { useState } from "react"
+export default function Strona3 (){
+
+const [color, setColor] = useState("bg-white")
+
+
+
+function changecolor(){
+
+setColor(color === "bg-black" ? "bg-white" : "bg-black") ///if
+
+}
+
+    return(
+        
+        <div className= {`${color} text-yellow-400 flex items-center justify-center border-2 gap-5`}>
+        
+        <h1>{}</h1>
+
+        <button onClick={changecolor}>Zmień color</button>
+        </div>
+        
+        
+
+
+
+    )
+} 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,6 +10,7 @@ return(
 <Link className='text-cyan-400'   href="/">Home</Link>
 <Link className='text-red-400' href="/strona1">Strona1</Link>
  <Link className='text-yellow-400' href="/strona2">Strona2</Link>
+ <Link classname ='text-green-400' href ="/strona3">Strona3</Link>
 </div>
 
 )
